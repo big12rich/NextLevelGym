ee208a8a8b856eb91ddcc6f18726d206c90080aa
chhowden <howden.char@uwlax.edu>
2014-05-07 16:22:41 -0500

    Thought I had R.java issue. Fixed it. Uploading now as backup in case of more problems. Damn.

--- src/com/HowdenCharles/nextlevelgymapp/LegPress.java
+++ src/com/HowdenCharles/nextlevelgymapp/LegPress.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
 import android.os.Build;
@@ -16,6 +21,26 @@ public class LegPress extends Activity {
 		setContentView(R.layout.activity_leg_press);
 		// Show the Up button in the action bar.
 		setupActionBar();
+		Spinner spinner1 = (Spinner) findViewById(R.id.spinner);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.sets_array, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner1.setAdapter(adapter);
+	}
+	public class SpinnerActivity extends Activity implements OnItemSelectedListener {
+	    
+	    public void onItemSelected(AdapterView<?> parent, View view, 
+	            int pos, long id) {
+	        // An item was selected. You can retrieve the selected item using
+	        // parent.getItemAtPosition(pos)
+	    }
+
+	    public void onNothingSelected(AdapterView<?> parent) {
+	        // Another interface callback
+	    }
 	}
 
 	/**
